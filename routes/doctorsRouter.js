579a4abd38644178131e2ad421471638bf793083
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-06-26 20:24:31 +0530

    appointments scheduling

--- routes/doctorsRouter.js
+++ routes/doctorsRouter.js
@@ -2,15 +2,18 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const { createReceptionist, createPatient, searchAndFetchUser } = require("../controllers/doctorController");
+const { createReceptionist, createPatient, searchAndFetchUser, getStaffByCreator, createSchedule, getSchedulesAndLeaves, createLeave } = require("../controllers/doctorController");
 const upload = multer({ dest: 'uploads/' }); // files go to ./uploads temporarily
 
 //Routes for approve Doctors
 router.post("/createReceptionist", upload.single('profilePic'), createReceptionist);
 router.get("/searchUser", searchAndFetchUser);
 router.post("/createPatient", createPatient);
-
-
+// Route definition
+router.get("/getStaffByCreator", getStaffByCreator);
+router.post("/createSchedule", createSchedule);
+router.post("/createLeave", createLeave);
+router.get("/getSchedulesAndLeaves", getSchedulesAndLeaves);
 
 
 module.exports = router;
