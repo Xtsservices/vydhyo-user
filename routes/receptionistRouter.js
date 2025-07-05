bceee507e285f0697a066647bb37195782dada4f
pavansannalaXTS <pavansannala@xtsservices.com>
2025-07-05 15:11:18 +0530

    new

--- routes/receptionistRouter.js
+++ routes/receptionistRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require('multer');
 
-const { updateReceptionist, fetchMyDoctors } = require("../controllers/receptionistController");
+const { updateReceptionist, fetchMyDoctors, fetchMyDoctorPatients } = require("../controllers/receptionistController");
 const upload = multer({ dest: 'uploads/' }); // files go to ./uploads temporarily
 
 router.get("/updateReceptionist", upload.single('profilePic'), updateReceptionist);
