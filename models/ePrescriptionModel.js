0c366bd43514bc60cb4c874c802bc06e8fcc5705
pavansannalaXTS <pavansannala@xtsservices.com>
2025-07-12 19:52:34 +0530

    attachments

--- models/ePrescriptionModel.js
+++ models/ePrescriptionModel.js
@@ -1,186 +1,192 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const eprescriptionSchema = new mongoose.Schema({
   prescriptionId: {
     type: String,
-    required: true
+    required: true,
+  },
+  prescriptionAttachment: {
+    type: String,
   },
   appointmentId: {
     type: String,
-    required: true
+    required: true,
   },
   userId: {
     type: String,
-    required: true
+    required: true,
   },
   doctorId: {
     type: String,
-    required: true
+    required: true,
   },
   addressId: {
     type: String,
-    required: true
+    required: true,
   },
   patientInfo: {
     patientName: {
       type: String,
-      required: true
+      required: true,
     },
     age: {
       type: Number,
-      required: true
+      required: true,
     },
     gender: {
       type: String,
-      required: true
+      required: true,
     },
     mobileNumber: {
       type: String,
-      required: true
+      required: true,
     },
     chiefComplaint: {
       type: String,
-      required: true
+      required: true,
     },
     pastMedicalHistory: {
       type: String,
-      default: null
+      default: null,
     },
     familyMedicalHistory: {
       type: String,
-      default: null
+      default: null,
     },
     physicalExamination: {
       type: String,
-      default: null
-    }
+      default: null,
+    },
   },
   vitals: {
     bp: {
       type: String,
-      default: null
+      default: null,
     },
     pulseRate: {
       type: String,
-      default: null
+      default: null,
     },
     respiratoryRate: {
       type: String,
-      default: null
+      default: null,
     },
     temperature: {
       type: String,
-      default: null
+      default: null,
     },
     spo2: {
       type: String,
-      default: null
+      default: null,
     },
     height: {
       type: String,
-      default: null
+      default: null,
     },
     weight: {
       type: String,
-      default: null
+      default: null,
     },
     bmi: {
       type: String,
-      default: null
+      default: null,
     },
     investigationFindings: {
       type: String,
-      default: null
-    }
+      default: null,
+    },
   },
   diagnosis: {
-    type: new mongoose.Schema({
-      diagnosisNote: {
-        type: String,
-        default: null
+    type: new mongoose.Schema(
+      {
+        diagnosisNote: {
+          type: String,
+          default: null,
+        },
+        testsNote: {
+          type: String,
+          default: null,
+        },
+        PrescribeMedNotes: {
+          type: String,
+          default: null,
+        },
+        selectedTests: [
+          {
+            testName: {
+              type: String,
+              required: true,
+            },
+            testInventoryId: {
+              type: String,
+              required: true,
+            },
+          },
+        ],
+        medications: [
+          {
+            medInventoryId: {
+              type: String,
+              required: true,
+            },
+            medName: {
+              type: String,
+              required: true,
+            },
+            quantity: {
+              type: Number,
+              required: true,
+            },
+            dosage: {
+              type: String,
+              required: true,
+            },
+            duration: {
+              type: Number,
+              required: true,
+            },
+            timings: [
+              {
+                type: String,
+              },
+            ],
+            frequency: {
+              type: Number,
+              required: true,
+            },
+          },
+        ],
       },
-      testsNote: {
-        type: String,
-        default: null
-      },
-      PrescribeMedNotes: {
-        type: String,
-        default: null
-      },
-      selectedTests: [
-        {
-          testName: {
-            type: String,
-            required: true
-          },
-          testInventoryId: {
-            type: String,
-            required: true
-          }
-        }
-      ],
-      medications: [
-        {
-          medInventoryId: {
-            type: String,
-            required: true
-          },
-          medName: {
-            type: String,
-            required: true
-          },
-          quantity: {
-            type: Number,
-            required: true
-          },
-          dosage: {
-            type: String,
-            required: true
-          },
-          duration: {
-            type: Number,
-            required: true
-          },
-          timings: [
-            {
-              type: String
-            }
-          ],
-          frequency: {
-            type: Number,
-            required: true
-          }
-        }
-      ]
-    }, { _id: false }),
-    default: null
+      { _id: false }
+    ),
+    default: null,
   },
   advice: {
     advice: {
       type: String,
-      default: null
+      default: null,
     },
     followUpDate: {
       type: String,
-      default: null
-    }
+      default: null,
+    },
   },
   createdBy: {
     type: String,
-    default: null
+    default: null,
   },
   updatedBy: {
     type: String,
-    default: null
+    default: null,
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   updatedAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('eprescriptions', eprescriptionSchema);
+module.exports = mongoose.model("eprescriptions", eprescriptionSchema);
