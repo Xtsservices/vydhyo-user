466ab6dd3b946db158d81fe7e14f1e95acc5b94d
Mkp991203 <manikumarkuyyeti@gmail.com>
2025-07-31 15:04:44 +0530

    removed images

--- queryBuilder/userAggregate.js
+++ queryBuilder/userAggregate.js
@@ -36,17 +36,26 @@ const userAggregation = (userId, excludeRefreshToken = false) => {
         path: '$insuranceDetails',
         preserveNullAndEmptyArrays: true
       }
+    },
+    {
+      $project: {
+        'specialization.drgreeCertificate': 0,
+        'specialization.specializationCertificate': 0,
+         'addresses.headerImage': 0,
+        'addresses.digitalSignature': 0,
+       ...(excludeRefreshToken && { refreshToken: 0 })
+      }
     }
   ];
 
   // Only add $project if exclusion is needed
-  if (excludeRefreshToken) {
-    pipeline.push({
-      $project: {
-        refreshToken: 0
-      }
-    });
-  }
+  // if (excludeRefreshToken) {
+  //   pipeline.push({
+  //     $project: {
+  //       refreshToken: 0
+  //     }
+  //   });
+  // }
 
   return pipeline;
 };
