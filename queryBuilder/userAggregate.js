ec40c613a13286a70f11d58f0b56e6a2d163a06e
sindhujaDyapaXTS <sindhu.d@xtsservices.com>
2025-09-14 22:40:07 +0530

    pipeline

--- queryBuilder/userAggregate.js
+++ queryBuilder/userAggregate.js
@@ -37,25 +37,25 @@ const userAggregation = (userId, excludeRefreshToken = false) => {
         preserveNullAndEmptyArrays: true
       }
     },
-    {
-      $project: {
-        // 'specialization.drgreeCertificate': 0,
-        // 'specialization.specializationCertificate': 0,
-        //  'addresses.headerImage': 0,
-        // 'addresses.digitalSignature': 0,
-       ...(excludeRefreshToken && { refreshToken: 0 })
-      }
-    }
+    // {
+    //   $project: {
+    //     // 'specialization.drgreeCertificate': 0,
+    //     // 'specialization.specializationCertificate': 0,
+    //     //  'addresses.headerImage': 0,
+    //     // 'addresses.digitalSignature': 0,
+    //    ...(excludeRefreshToken && { refreshToken: 0 })
+    //   }
+    // }
   ];
 
   // Only add $project if exclusion is needed
-  // if (excludeRefreshToken) {
-  //   pipeline.push({
-  //     $project: {
-  //       refreshToken: 0
-  //     }
-  //   });
-  // }
+  if (excludeRefreshToken) {
+    pipeline.push({
+      $project: {
+        refreshToken: 0
+      }
+    });
+  }
 
   return pipeline;
 };
